6e29dc152c19f58934093951cb5763c4a540f010
zkroliko <zbkrolikowski@gmail.com>
2017-12-04 22:07:13 +0100

    Tweak normalization to operate properly withing bound of numeric error

--- analyze.py
+++ analyze.py
@@ -7,13 +7,12 @@ TEST_ROWS = 0
 training_filename = 'training_data.csv'
 test_filename = 'test_data.csv'
 
-NORMALIZED = False
+NORMALIZED = True
 
 (x_train, y_train), (x_test, y_test) = load_data(training_filename, TRAIN_ROWS, normalized=NORMALIZED),\
                                        load_data(test_filename, TEST_ROWS, normalized=NORMALIZED)
 
 data = np.vstack((x_train,x_test))
-print(data)
 print("Minimums:")
 print(data.min(axis=0))
 print("Maximums:")
