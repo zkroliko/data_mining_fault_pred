917bd8996965e8049d32deca4e3f8bceb574e55f
zkroliko <zbkrolikowski@gmail.com>
2017-12-18 19:27:09 +0100

    A function for making tensors suitable as input for CNN

---
+++ transform_input.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+
+def make_timeseries_instances(timeseries, window_size):
+    """Make input features and prediction targets from a `timeseries` for use in machine learning.
+    :return: A tuple of `(X, y, q)`.  `X` are the inputs to a predictor, a 3D ndarray with shape
+      ``(timeseries.shape[0] - window_size, window_size, timeseries.shape[1] or 1)``.  For each row of `X`, the
+      corresponding row of `y` is the next value in the timeseries.  The `q` or query is the last instance, what you would use
+      to predict a hypothetical next (unprovided) value in the `timeseries`.
+    :param ndarray timeseries: Either a simple vector, or a matrix of shape ``(timestep, series_num)``, i.e., time is axis 0 (the
+      row) and the series is axis 1 (the column).
+    :param int window_size: The number of samples to use as input prediction features (also called the lag or lookback).
+    """
+    timeseries = np.asarray(timeseries)
+    assert 0 < window_size < timeseries.shape[0]
+    X = np.atleast_3d(
+        np.array([timeseries[start:start + window_size] for start in range(0, timeseries.shape[0] - window_size)]))
+    y = timeseries[window_size:]
+    q = np.atleast_3d([timeseries[-window_size:]])
+    return X
